917538aa204d23ccf84fe641e4047aacafe459ed
Bruno <brunomarcaldossantos9a@gmail.com>
2023-10-14 11:36:51 -0300

    Modal

--- JS/main.js
+++ JS/main.js
@@ -23,8 +23,88 @@ const cardapioRef = ref(db, "Cardapio");
 
 // Função para editar um item
 function editItem(cardapioKey) {
-    // Implemente a função de edição aqui
-    // Você pode usar um modal semelhante ao código anterior
+  // ...
+
+// Função para abrir o modal de edição
+function openEditModal(cardapioKey) {
+    const item = cardapioData[cardapioKey]; // Obtenha os dados do item com base na chave
+    document.getElementById("editNome").value = item.nome;
+    document.getElementById("editCategoria").value = item.categoria;
+    document.getElementById("editDescricao").value = item.descricao;
+    document.getElementById("editPreco").value = item.preco;
+    document.getElementById("editModal").style.display = "block";
+    document.getElementById("editCardapioKey").value = cardapioKey; // Salve a chave do item
+
+    // Preencha a imagem atual do item
+    const currentImage = document.getElementById("currentImage");
+    currentImage.src = item.imagem;
+
+    // Limpe o campo de upload de imagem
+    document.getElementById("editImagem").value = "";
+}
+
+// Função para salvar as alterações
+function saveChanges() {
+    const cardapioKey = document.getElementById("editCardapioKey").value;
+    const novoNome = document.getElementById("editNome").value;
+    const novaCategoria = document.getElementById("editCategoria").value;
+    const novaDescricao = document.getElementById("editDescricao").value;
+    const novoPreco = document.getElementById("editPreco").value;
+
+    // Obtenha o arquivo de imagem selecionado pelo usuário
+    const novaImagem = document.getElementById("editImagem").files[0];
+
+    if (novaImagem) {
+        // Realize o upload da nova imagem para o Firebase Storage e obtenha a URL
+        const storage = getStorage(app);
+        const storageRef = ref(storage, 'imagens/' + novaImagem.name);
+        uploadBytes(storageRef, novaImagem).then((snapshot) => {
+            getDownloadURL(snapshot.ref).then((downloadURL) => {
+                // Atualize os dados no Firebase, incluindo a nova URL da imagem
+                const cardapioRef = ref(db, "Cardapio/" + cardapioKey);
+                const updates = {
+                    nome: novoNome,
+                    categoria: novaCategoria,
+                    descricao: novaDescricao,
+                    preco: novoPreco,
+                    imagem: downloadURL, // URL da nova imagem
+                };
+
+                // Atualize os dados no Firebase com as novas informações
+                update(cardapioRef, updates).then(() => {
+                    alert("Alterações salvas com sucesso!");
+                    closeEditModal(); // Feche o modal após salvar as alterações
+                    loadCardapioData(); // Recarregue os dados na tabela
+                }).catch((error) => {
+                    console.error("Erro ao salvar as alterações: " + error);
+                });
+            });
+        }).catch((error) => {
+            console.error("Erro ao fazer upload da imagem: " + error);
+        });
+    } else {
+        // Se o usuário não selecionou uma nova imagem, atualize os dados sem a imagem
+        const cardapioRef = ref(db, "Cardapio/" + cardapioKey);
+        const updates = {
+            nome: novoNome,
+            categoria: novaCategoria,
+            descricao: novaDescricao,
+            preco: novoPreco,
+        };
+
+        // Atualize os dados no Firebase com as novas informações
+        update(cardapioRef, updates).then(() => {
+            alert("Alterações salvas com sucesso!");
+            closeEditModal(); // Feche o modal após salvar as alterações
+            loadCardapioData(); // Recarregue os dados na tabela
+        }).catch((error) => {
+            console.error("Erro ao salvar as alterações: " + error);
+        });
+    }
+}
+
+// ...
+
 }
 
 // Função para deletar um item
@@ -68,6 +148,8 @@ function loadCardapioData() {
                         <td>${item.descricao}</td>
                         <td>${item.preco}</td>
                         <td>
+                    
+                            button class="editar-button" data-key="${key}">Excluir</button>
                             <button class="delete-button" data-key="${key}">Excluir</button>
                         </td>
                     `;
