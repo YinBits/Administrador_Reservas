f49a25ce85ce4bc8b8b0f93543405c0923700d41
Bruno <brunomarcaldossantos9a@gmail.com>
2023-10-14 13:21:35 -0300

    FuncionalDelete

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 // Importar as funções necessárias do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getDatabase, ref, get, remove, update } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+import { getDatabase, ref, get, remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
 // Configuração do Firebase (substitua pelos seus próprios valores)
 const firebaseConfig = {
@@ -21,86 +21,10 @@ const db = getDatabase(app);
 // Referência para o nó "Cardapio" no Realtime Database
 const cardapioRef = ref(db, "Cardapio");
 
-// Função para abrir o modal de edição
-function openEditModal(cardapioKey) {
-    const editModal = document.getElementById("editModal");
-    editModal.style.display = "block";
-
-    const editImage = document.getElementById("editImagePreview");
-    const editImagemInput = document.getElementById("editImagem");
-
-    // Carregue os dados do Firebase com base na chave 'cardapioKey'
-    // e preencha o formulário de edição com os dados do item
-    const itemRef = ref(db, `Cardapio/${cardapioKey}`);
-    get(itemRef).then((snapshot) => {
-        if (snapshot.exists()) {
-            const item = snapshot.val();
-            document.getElementById("editCardapioKey").value = cardapioKey;
-            document.getElementById("editNome").value = item.nome;
-            document.getElementById("editCategoria").value = item.categoria;
-            document.getElementById("editDescricao").value = item.descricao;
-            document.getElementById("editPreco").value = item.preco;
-
-            // Exiba a imagem atual
-            editImage.src = item.imagem;
-        } else {
-            console.error("Item não encontrado no banco de dados.");
-        }
-    }).catch((error) => {
-        console.error("Erro ao obter os dados do item: " + error);
-    });
-
-    // Ouvinte de evento para o campo de imagem para exibir uma visualização da nova imagem
-    editImagemInput.addEventListener("change", (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onload = (e) => {
-                editImage.src = e.target.result;
-            };
-            reader.readAsDataURL(file);
-        }
-    });
-
-    // Ouvinte de evento para o botão "Salvar Alterações"
-    const saveButton = document.getElementById("editSaveButton");
-    saveButton.addEventListener("click", () => {
-        saveChanges(cardapioKey);
-    });
-}
-
-// Função para salvar as alterações
-function saveChanges(cardapioKey) {
-    const nome = document.getElementById("editNome").value;
-    const categoria = document.getElementById("editCategoria").value;
-    const descricao = document.getElementById("editDescricao").value;
-    const preco = document.getElementById("editPreco").value;
-
-    // Verifique se todos os campos foram preenchidos
-    if (!nome || !categoria || !descricao || !preco) {
-        alert("Por favor, preencha todos os campos.");
-        return;
-    }
-
-    // Atualize os dados no Realtime Database
-    const cardapioRefKey = ref(db, "Cardapio/" + cardapioKey);
-    update(cardapioRefKey, {
-        nome: nome,
-        categoria: categoria,
-        descricao: descricao,
-        preco: preco
-    }).then(() => {
-        alert("Dados atualizados com sucesso.");
-        closeEditModal();
-    }).catch((error) => {
-        console.error("Erro ao atualizar dados: " + error);
-    });
-}
-
-// Função para fechar o modal de edição
-function closeEditModal() {
-    const editModal = document.getElementById("editModal");
-    editModal.style.display = "none";
+// Função para editar um item
+function editItem(cardapioKey) {
+    // Implemente a função de edição aqui
+    // Você pode usar um modal semelhante ao código anterior
 }
 
 // Função para deletar um item
@@ -108,12 +32,22 @@ function deleteItem(cardapioKey) {
     const cardapioRef = ref(db, "Cardapio/" + cardapioKey);
     remove(cardapioRef).then(() => {
         alert("Item deletado com sucesso!");
-        // Recarregue os dados após a exclusão
+        // Recarregue os dados após a exclusão (você pode criar uma função separada para isso)
         loadCardapioData();
     }).catch((error) => {
         console.error("Erro ao deletar o item: " + error);
     });
 }
+
+// Adicione um ouvinte de evento aos botões de exclusão
+document.querySelectorAll(".delete-button").forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const cardapioKey = event.target.getAttribute("data-key");
+        if (cardapioKey) {
+            deleteItem(cardapioKey);
+        }
+    });
+});
 
 // Função para carregar os dados do Firebase e exibi-los na tabela
 function loadCardapioData() {
@@ -134,13 +68,22 @@ function loadCardapioData() {
                         <td>${item.descricao}</td>
                         <td>${item.preco}</td>
                         <td>
-                            <button class="delete-button" data-key="${key}" onclick="deleteItem('${key}')">Excluir</button>
-                            <button class="edit-button" data-key="${key}" onclick="openEditModal('${key}')">Editar</button>
+                            <button class="delete-button" data-key="${key}">Excluir</button>
                         </td>
                     `;
                     cardapioTableBody.appendChild(newRow);
                 }
             }
+
+            // Adicione um ouvinte de evento aos botões de exclusão
+            document.querySelectorAll(".delete-button").forEach((button) => {
+                button.addEventListener("click", (event) => {
+                    const cardapioKey = event.target.getAttribute("data-key");
+                    if (cardapioKey) {
+                        deleteItem(cardapioKey);
+                    }
+                });
+            });
         }
     }).catch((error) => {
         console.error("Erro ao obter os dados: " + error);
