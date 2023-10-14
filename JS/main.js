71dc2baa2ba3c6cc7c5af4ce5138fe9551b2c27a
Bruno <brunomarcaldossantos9a@gmail.com>
2023-10-14 13:06:25 -0300

    EditModal

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 // Importar as funções necessárias do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getDatabase, ref, get, remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
+import { getDatabase, ref, get, update, remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
 // Configuração do Firebase (substitua pelos seus próprios valores)
 const firebaseConfig = {
@@ -21,23 +21,92 @@ const db = getDatabase(app);
 // Referência para o nó "Cardapio" no Realtime Database
 const cardapioRef = ref(db, "Cardapio");
 
-// Função para editar um item
-function editItem(cardapioKey) {
-    // Implemente a função de edição aqui
-    // Você pode usar um modal semelhante ao código anterior
+// Função para abrir o modal de edição
+function openEditModal(cardapioKey) {
+    const editModal = document.getElementById("editModal");
+    const editCardapioKey = document.getElementById("editCardapioKey");
+    const editNome = document.getElementById("editNome");
+    const editCategoria = document.getElementById("editCategoria");
+    const editDescricao = document.getElementById("editDescricao");
+    const editPreco = document.getElementById("editPreco");
+
+    // Carregue os dados do Firebase com base na chave 'cardapioKey'
+    const itemRef = ref(db, `Cardapio/${cardapioKey}`);
+    get(itemRef).then((snapshot) => {
+        if (snapshot.exists()) {
+            const item = snapshot.val();
+            editCardapioKey.value = cardapioKey;
+            editNome.value = item.nome;
+            editCategoria.value = item.categoria;
+            editDescricao.value = item.descricao;
+            editPreco.value = item.preco;
+
+            // Abra o modal de edição
+            editModal.style.display = "block";
+        } else {
+            console.error("Item não encontrado no banco de dados.");
+        }
+    }).catch((error) => {
+        console.error("Erro ao obter os dados do item: " + error);
+    });
+}
+
+// Função para salvar as alterações
+function saveChanges() {
+    const cardapioKey = document.getElementById("editCardapioKey").value;
+    const nome = document.getElementById("editNome").value;
+    const categoria = document.getElementById("editCategoria").value;
+    const descricao = document.getElementById("editDescricao").value;
+    const preco = document.getElementById("editPreco").value;
+
+    // Verifique se todos os campos foram preenchidos
+    if (!nome || !categoria || !descricao || !preco) {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    }
+
+    // Atualize os dados no Realtime Database
+    const cardapioRefKey = ref(db, `Cardapio/${cardapioKey}`);
+    update(cardapioRefKey, {
+        nome: nome,
+        categoria: categoria,
+        descricao: descricao,
+        preco: preco
+    }).then(() => {
+        alert("Dados atualizados com sucesso.");
+        closeEditModal();
+    }).catch((error) => {
+        console.error("Erro ao atualizar dados: " + error);
+    });
+}
+
+// Função para fechar o modal de edição
+function closeEditModal() {
+    const editModal = document.getElementById("editModal");
+    editModal.style.display = "none";
 }
 
 // Função para deletar um item
 function deleteItem(cardapioKey) {
-    const cardapioRef = ref(db, "Cardapio/" + cardapioKey);
-    remove(cardapioRef).then(() => {
+    const cardapioRefKey = ref(db, `Cardapio/${cardapioKey}`);
+    remove(cardapioRefKey).then(() => {
         alert("Item deletado com sucesso!");
-        // Recarregue os dados após a exclusão (você pode criar uma função separada para isso)
+        // Recarregue os dados após a exclusão
         loadCardapioData();
     }).catch((error) => {
         console.error("Erro ao deletar o item: " + error);
     });
 }
+
+// Adicione um ouvinte de evento aos botões de edição
+document.querySelectorAll(".edit-button").forEach((button) => {
+    button.addEventListener("click", (event) => {
+        const cardapioKey = event.target.getAttribute("data-key");
+        if (cardapioKey) {
+            openEditModal(cardapioKey);
+        }
+    });
+});
 
 // Adicione um ouvinte de evento aos botões de exclusão
 document.querySelectorAll(".delete-button").forEach((button) => {
@@ -62,28 +131,18 @@ function loadCardapioData() {
                     const item = cardapioData[key];
                     const newRow = document.createElement("tr");
                     newRow.innerHTML = `
-                        <td><img src="${item.imagem}" class="thumbnail"></td>
                         <td>${item.nome}</td>
                         <td>${item.categoria}</td>
                         <td>${item.descricao}</td>
                         <td>${item.preco}</td>
                         <td>
+                            <button class="edit-button" data-key="${key}">Editar</button>
                             <button class="delete-button" data-key="${key}">Excluir</button>
                         </td>
                     `;
                     cardapioTableBody.appendChild(newRow);
                 }
             }
-
-            // Adicione um ouvinte de evento aos botões de exclusão
-            document.querySelectorAll(".delete-button").forEach((button) => {
-                button.addEventListener("click", (event) => {
-                    const cardapioKey = event.target.getAttribute("data-key");
-                    if (cardapioKey) {
-                        deleteItem(cardapioKey);
-                    }
-                });
-            });
         }
     }).catch((error) => {
         console.error("Erro ao obter os dados: " + error);
