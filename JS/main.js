ece1ac6ff2941455f36a52ac15ecd30a025d880a
Bruno <brunomarcaldossantos9a@gmail.com>
2023-10-14 12:37:25 -0300

    EditModal

--- JS/main.js
+++ JS/main.js
@@ -21,10 +21,47 @@ const db = getDatabase(app);
 // Referência para o nó "Cardapio" no Realtime Database
 const cardapioRef = ref(db, "Cardapio");
 
-// Função para editar um item
-function editItem(cardapioKey) {
-    // Implemente a função de edição aqui
-    // Você pode usar um modal semelhante ao código anterior
+// Função para abrir o modal de edição
+function openEditModal(cardapioKey) {
+    const editModal = document.getElementById("editModal");
+    editModal.style.display = "block";
+
+    const editImage = document.getElementById("editImagePreview");
+    const editImagemInput = document.getElementById("editImagem");
+
+    // Carregue os dados do Firebase com base na chave 'cardapioKey'
+    // e preencha o formulário de edição com os dados do item
+    const itemRef = ref(db, `Cardapio/${cardapioKey}`);
+    get(itemRef).then((snapshot) => {
+        if (snapshot.exists()) {
+            const item = snapshot.val();
+            document.getElementById("editCardapioKey").value = cardapioKey;
+            document.getElementById("editNome").value = item.nome;
+            document.getElementById("editCategoria").value = item.categoria;
+            document.getElementById("editDescricao").value = item.descricao;
+            document.getElementById("editPreco").value = item.preco;
+            // Preencha outros campos conforme necessário
+
+            // Exiba a imagem atual
+            editImage.src = item.imagem;
+        } else {
+            console.error("Item não encontrado no banco de dados.");
+        }
+    }).catch((error) => {
+        console.error("Erro ao obter os dados do item: " + error);
+    });
+
+    // Ouvinte de evento para o campo de imagem para exibir uma visualização da nova imagem
+    editImagemInput.addEventListener("change", (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                editImage.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
+        }
+    });
 }
 
 // Função para deletar um item
@@ -69,6 +106,7 @@ function loadCardapioData() {
                         <td>${item.preco}</td>
                         <td>
                             <button class="delete-button" data-key="${key}">Excluir</button>
+                            <button class="edit-button" data-key="${key}">Editar</button>
                         </td>
                     `;
                     cardapioTableBody.appendChild(newRow);
@@ -81,6 +119,16 @@ function loadCardapioData() {
                     const cardapioKey = event.target.getAttribute("data-key");
                     if (cardapioKey) {
                         deleteItem(cardapioKey);
+                    }
+                });
+            });
+
+            // Adicione um ouvinte de evento aos botões de edição
+            document.querySelectorAll(".edit-button").forEach((button) => {
+                button.addEventListener("click", (event) => {
+                    const cardapioKey = event.target.getAttribute("data-key");
+                    if (cardapioKey) {
+                        openEditModal(cardapioKey);
                     }
                 });
             });
