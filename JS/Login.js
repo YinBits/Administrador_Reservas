25891c0fe7edfac5fd93ea870362fb9f037e5374
Bruno <brunomarcaldossantos9a@gmail.com>
2023-10-17 11:35:00 -0300

    Update Login.js

--- JS/Login.js
+++ JS/Login.js
@@ -1,35 +1,38 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-analytics.js";
+// Importe os módulos do Firebase a partir de URLs corretas
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
+
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyABfDBGL-M3oDLg6JGH79OksO45LdErczM",
-    authDomain: "tinareactnativefirebase.firebaseapp.com",
-    databaseURL: "https://tinareactnativefirebase-default-rtdb.firebaseio.com",
-    projectId: "tinareactnativefirebase",
-    storageBucket: "tinareactnativefirebase.appspot.com",
-    messagingSenderId: "285805953156",
-    appId: "1:285805953156:web:5451de5f8f99779571c2d1",
-    measurementId: "G-1K94YJ5G55"
-  };
-  
-  // Inicialize o Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  
-  // Adicione um ouvinte de evento para o formulário de login
-  document.getElementById("loginForm").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-  
-    // Autenticação com Firebase
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((userCredential) => {
-        // Autenticação bem-sucedida
-        window.location.href = "./inicio.html"; // Redireciona para a página "Início"
-      })
-      .catch((error) => {
-        alert("Credenciais incorretas. Tente novamente.");
-      });
-  });
-  
+  apiKey: "AIzaSyABfDBGL-M3oDLg6JGH79OksO45LdErczM",
+  authDomain: "tinareactnativefirebase.firebaseapp.com",
+  databaseURL: "https://tinareactnativefirebase-default-rtdb.firebaseio.com",
+  projectId: "tinareactnativefirebase",
+  storageBucket: "tinareactnativefirebase.appspot.com",
+  messagingSenderId: "285805953156",
+  appId: "1:285805953156:web:5451de5f8f99779571c2d1",
+  measurementId: "G-1K94YJ5G55"
+};
+
+// Inicialize o Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+// Adicione um ouvinte de evento para o formulário de login
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // Autenticação com Firebase
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Autenticação bem-sucedida
+      window.location.href = "./inicio.html"; // Redireciona para a página "Início"
+    })
+    .catch((error) => {
+      alert("Credenciais incorretas. Tente novamente.");
+    });
+});
