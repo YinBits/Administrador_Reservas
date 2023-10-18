bf04236f5828d11678ac54e92243fc350b1db487
Bruno <brunomarcaldossantos9a@gmail.com>
2023-10-18 08:47:24 -0300

    Reserva
    Inclusão das reservas do app

--- JS/main-reserva.js
+++ JS/main-reserva.js
@@ -1,3 +1,4 @@
+// Importações Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
 import { getDatabase, ref, push, set, get, remove } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js";
 
@@ -13,10 +14,12 @@ const firebaseConfig = {
     measurementId: "G-1K94YJ5G55"
 };
 
+// Inicialização do Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const reservasRef = ref(db, "Reservas");
 
+// Função para formatar a data
 function formatDate(date) {
     const d = new Date(date);
     const day = d.getDate().toString().padStart(2, '0');
@@ -25,11 +28,13 @@ function formatDate(date) {
     return `${day}/${month}/${year}`;
 }
 
+// Função para abrir o modal de edição
 function openEditModal() {
     const editModal = document.getElementById("editModal");
     editModal.style.display = "block";
 }
 
+// Função para editar reserva
 function editReserva(key) {
     const reservaItemRef = ref(db, "Reservas/" + key);
 
@@ -76,6 +81,7 @@ function editReserva(key) {
         });
 }
 
+// Função para excluir reserva
 function deleteReserva(key) {
     const reservaRef = ref(db, "Reservas/" + key);
     remove(reservaRef)
@@ -88,62 +94,70 @@ function deleteReserva(key) {
         });
 }
 
+// Função para fechar o modal de edição
 function closeEditModal() {
     const editModal = document.getElementById("editModal");
     editModal.style.display = "none";
 }
 
-function loadReservasData() {
+// Função para montar a tabela com as reservas
+function montarTabela(reservasData) {
     const today = new Date();
+    const reservasTableBody = document.getElementById("ReservasTableBody");
+    reservasTableBody.innerHTML = "";
+
+    for (const key in reservasData) {
+        if (reservasData.hasOwnProperty(key)) {
+            const reserva = reservasData[key];
+            const dataReserva = new Date(reserva.dataReserva);
+
+            if (dataReserva >= today) {
+                const newRow = document.createElement("tr");
+                newRow.innerHTML = `
+                    <td>${formatDate(dataReserva)}</td>
+                    <td>${reserva.nomeCliente}</td> // Substitua pelo nome do cliente
+                    <td>${reserva.horarioReserva}</td>
+                    <td>${reserva.numeroMesa}</td>
+                    <td>${reserva.numeroPessoas}</td>
+                    <td>
+                        <button class="edit-button" data-key="${key}">Editar</button>
+                        <button class="delete-button" data-key="${key}">Excluir</button>
+                    </td>
+                `;
+                reservasTableBody.appendChild(newRow);
+            }
+        }
+    }
+
+    // Adicione aqui a lógica para ouvir os eventos de edição e exclusão, semelhante ao seu código original.
+    document.querySelectorAll(".delete-button").forEach((button) => {
+        button.addEventListener("click", (event) => {
+            const key = event.target.getAttribute("data-key");
+            if (key) {
+                if (confirm("Tem certeza de que deseja excluir esta reserva?")) {
+                    deleteReserva(key);
+                }
+            }
+        });
+    });
+
+    document.querySelectorAll(".edit-button").forEach((button) => {
+        button.addEventListener("click", (event) => {
+            const key = event.target.getAttribute("data-key");
+            if (key) {
+                editReserva(key);
+            }
+        });
+    });
+}
+
+// Função para carregar as reservas existentes ao inicializar a página
+function loadReservasData() {
     get(reservasRef)
         .then((snapshot) => {
             if (snapshot.exists()) {
                 const reservasData = snapshot.val();
-                const reservasTableBody = document.getElementById("ReservasTableBody");
-                reservasTableBody.innerHTML = "";
-
-                for (const key in reservasData) {
-                    if (reservasData.hasOwnProperty(key)) {
-                        const reserva = reservasData[key];
-                        const dataReserva = new Date(reserva.dataReserva);
-
-                        if (dataReserva >= today) {
-                            const newRow = document.createElement("tr");
-                            newRow.innerHTML = `
-                                <td>${formatDate(dataReserva)}</td>
-                                <td>${reserva.nomeCliente}</td>
-                                <td>${reserva.horarioReserva}</td>
-                                <td>${reserva.numeroMesa}</td>
-                                <td>${reserva.numeroPessoas}</td>
-                                <td>
-                                    <button class="edit-button" data-key="${key}">Editar</button>
-                                    <button class="delete-button" data-key="${key}">Excluir</button>
-                                </td>
-                            `;
-                            reservasTableBody.appendChild(newRow);
-                        }
-                    }
-                }
-
-                document.querySelectorAll(".delete-button").forEach((button) => {
-                    button.addEventListener("click", (event) => {
-                        const key = event.target.getAttribute("data-key");
-                        if (key) {
-                            if (confirm("Tem certeza de que deseja excluir esta reserva?")) {
-                                deleteReserva(key);
-                            }
-                        }
-                    });
-                });
-
-                document.querySelectorAll(".edit-button").forEach((button) => {
-                    button.addEventListener("click", (event) => {
-                        const key = event.target.getAttribute("data-key");
-                        if (key) {
-                            editReserva(key);
-                        }
-                    });
-                });
+                montarTabela(reservasData); // Chama a função para montar a tabela com as reservas
             }
         })
         .catch((error) => {
@@ -151,4 +165,5 @@ function loadReservasData() {
         });
 }
 
+// Chame a função para carregar as reservas existentes ao inicializar a página.
 loadReservasData();
